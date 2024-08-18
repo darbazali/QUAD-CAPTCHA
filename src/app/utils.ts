daa2b62b101cc603fc733f9d883657e44e57377e
Interceptor <darbaz.me@gmail.com>
2024-08-18 17:10:07 +0300

    Add JSDoc comments to `getRandomInteger` function in utils.ts

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,3 +1,9 @@
-const getRandomInteger = (minimum: number, maximum: number): number => {
-  return minimum + Math.floor(Math.random() * (maximum - minimum + 1));
+/**
+ * Returns a random integer between the given minimum and maximum values, inclusive.
+ * @param {number} min The minimum value.
+ * @param {number} max The maximum value.
+ * @return {number} A random integer between min and max, inclusive.
+ */
+const getRandomInteger = (min: number, max: number): number => {
+  return min + Math.floor(Math.random() * (max - min + 1));
 };
